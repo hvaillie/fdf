2ad0316e9c23dbd38952cb63ba9478e638ccac54
Hugues VAILLIER <hvaillie@e1r2p10.42.fr>
2018-01-24 19:54:48 +0100

    Premier nuage de point bleu

--- fdf.h
+++ fdf.h
@@ -13,6 +13,17 @@
 #ifndef FDF_H
 # define FDF_H
 
-#define ESCAPE_KEY 53
+# define ESCAPE_KEY 53
+# define RED 	0xFF0000
+# define GREEN 	0x00FF00
+# define BLUE 	0x0000FF
+# define BLACK	0x000000
+# define WHITE	0xFFFFFF
+
+typedef struct	s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_mlx;
 
 #endif
